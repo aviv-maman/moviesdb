8abd30c59c37c87b31c0a8ea1729a7d7ef11b863
Aviv Maman <awiwmaman@outlook.com>
2023-08-26 18:01:32 +0300

    features

--- app/page.tsx
+++ app/page.tsx
@@ -7,29 +7,9 @@ import topMovies from '@/lib/dummy-data/top_rated-movies.json';
 import upcomingMovies from '@/lib/dummy-data/upcoming-movies.json';
 import type { ListResponse, UpcomingListResponse } from '@/lib/api.types';
 import FeatureCard from '@/components/FeatureCard';
+import { features, resources } from '@/lib/features-data';
 
 export const dynamic = 'force-dynamic';
-
-const resources = [
-  {
-    title: 'Cookie-based Auth and the Next.js App Router',
-    subtitle: 'This free course by Jon Meyers, shows you how to configure Supabase Auth to use cookies, and steps through some common patterns.',
-    url: 'https://youtube.com/playlist?list=PL5S4mPUpp4OtMhpnp93EFSo42iQ40XjbF',
-    icon: 'M7 4V20M17 4V20M3 8H7M17 8H21M3 12H21M3 16H7M17 16H21M4 20H20C20.5523 20 21 19.5523 21 19V5C21 4.44772 20.5523 4 20 4H4C3.44772 4 3 4.44772 3 5V19C3 19.5523 3.44772 20 4 20Z',
-  },
-  {
-    title: 'Supabase Next.js App Router Example',
-    subtitle: 'Want to see a code example containing some common patterns with Next.js and Supabase? Check out this repo!',
-    url: 'https://github.com/supabase/supabase/tree/master/examples/auth/nextjs',
-    icon: 'M10 20L14 4M18 8L22 12L18 16M6 16L2 12L6 8',
-  },
-  {
-    title: 'Supabase Auth Helpers Docs',
-    subtitle: 'This template has configured Supabase Auth to use cookies for you, but the docs are a great place to learn more.',
-    url: 'https://supabase.com/docs/guides/auth/auth-helpers/nextjs',
-    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
-  },
-];
 
 export default async function Index() {
   const trendingData = trendingMedia as ListResponse;
@@ -104,10 +84,9 @@ export default async function Index() {
         </div>
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-          <FeatureCard />
-          <FeatureCard />
-          <FeatureCard />
-          <FeatureCard />
+          {features.map(({ title, subtitle, icon }) => (
+            <FeatureCard key={title} title={title} subtitle={subtitle} icon={icon} />
+          ))}
         </div>
 
         <div className='justify-center flex flex-col gap-7 text-xs'>
---
+++ lib/features-data.ts
@@ -0,0 +1,76 @@
+export const resources = [
+  {
+    title: 'Cookie-based Auth and the Next.js App Router',
+    subtitle: 'This free course by Jon Meyers, shows you how to configure Supabase Auth to use cookies, and steps through some common patterns.',
+    url: 'https://youtube.com/playlist?list=PL5S4mPUpp4OtMhpnp93EFSo42iQ40XjbF',
+    icon: 'M7 4V20M17 4V20M3 8H7M17 8H21M3 12H21M3 16H7M17 16H21M4 20H20C20.5523 20 21 19.5523 21 19V5C21 4.44772 20.5523 4 20 4H4C3.44772 4 3 4.44772 3 5V19C3 19.5523 3.44772 20 4 20Z',
+  },
+  {
+    title: 'Supabase Next.js App Router Example',
+    subtitle: 'Want to see a code example containing some common patterns with Next.js and Supabase? Check out this repo!',
+    url: 'https://github.com/supabase/supabase/tree/master/examples/auth/nextjs',
+    icon: 'M10 20L14 4M18 8L22 12L18 16M6 16L2 12L6 8',
+  },
+  {
+    title: 'Supabase Auth Helpers Docs',
+    subtitle: 'This template has configured Supabase Auth to use cookies for you, but the docs are a great place to learn more.',
+    url: 'https://supabase.com/docs/guides/auth/auth-helpers/nextjs',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+];
+
+export const features = [
+  {
+    title: 'Add to watch lists',
+    subtitle: 'Add movies to your watch list and keep track of what you want to watch next.',
+    icon: ['M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z', 'M9 7l6 0', 'M9 11l6 0', 'M9 15l4 0'],
+  },
+  {
+    title: 'Get recommendations',
+    subtitle: 'Get recommendations based on your watch list and favorites list.',
+    icon: [
+      'M17.8 19.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z',
+      'M6.2 19.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z',
+      'M12 9.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z',
+    ],
+  },
+  {
+    title: 'Rate movies',
+    subtitle: 'Keep track of what you thought of a movie by rating it on a scale of 1 to 5 stars.',
+    icon: ['M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z'],
+  },
+  {
+    title: 'Search for movies',
+    subtitle: 'Search for movies by title, genre, or actor.',
+    icon: [
+      'M14 3.072a8 8 0 0 1 2.617 11.424l4.944 4.943a1.5 1.5 0 0 1 -2.008 2.225l-.114 -.103l-4.943 -4.944a8 8 0 0 1 -12.49 -6.332l-.006 -.285l.005 -.285a8 8 0 0 1 11.995 -6.643z',
+    ],
+  },
+  {
+    title: 'View movie details',
+    subtitle: 'View details about a movie including the cast, crew, and reviews.',
+    icon: [
+      'M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2',
+      'M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z',
+      'M9 12l.01 0',
+      'M13 12l2 0',
+      'M9 16l.01 0',
+      'M13 16l2 0',
+    ],
+  },
+  {
+    title: 'Discover new movies',
+    subtitle: 'Discover new movies by browsing through the top rated, most popular, and upcoming movies.',
+    icon: ['M8 16l2 -6l6 -2l-2 6l-6 2', 'M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0', 'M12 3l0 2', 'M12 19l0 2', 'M3 12l2 0', 'M19 12l2 0'],
+  },
+  {
+    title: 'Link streaming services',
+    subtitle: 'Filter by your subscribed streaming services and find something to watch.',
+    icon: ['M3 19l.01 0', 'M7 19a4 4 0 0 0 -4 -4', 'M11 19a8 8 0 0 0 -8 -8', 'M15 19h3a3 3 0 0 0 3 -3v-8a3 3 0 0 0 -3 -3h-12a3 3 0 0 0 -2.8 2'],
+  },
+  {
+    title: 'View movie trailers',
+    subtitle: 'Watch trailers of movies and series.',
+    icon: ['M7 4v16l13 -8z'],
+  },
+];
