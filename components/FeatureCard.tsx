cf303381da4342b69e24f923eb8a4f8cde664013
Aviv Maman <awiwmaman@outlook.com>
2023-08-26 15:34:36 +0300

    cards

--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -26,14 +26,14 @@ const FeatureCard: FC<FeatureCardProps> = ({}) => {
   return (
     <div
       ref={containerRef}
-      className='card flex flex-col relative overflow-hidden height-auto text-foreground box-border outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large transition-transform-background motion-reduce:transition-none border-transparent bg-white/5 dark:bg-default-400/10 backdrop-blur-lg backdrop-saturate-[1.8]'
+      className='card flex flex-col relative overflow-hidden height-auto text-foreground box-border outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large transition-transform-background motion-reduce:transition-none border bg-white/5 dark:bg-default-400/10 backdrop-blur-lg backdrop-saturate-[1.8]'
       tabIndex={-1}>
       <div className='flex p-3 z-10 w-full justify-start items-center shrink-0 overflow-inherit color-inherit subpixel-antialiased rounded-t-large gap-2 pb-0'>
         <div className='flex justify-center p-2 rounded-full items-center bg-secondary-100/80 text-pink-500'>SVG</div>
         <p className='text-base font-semibold'>React server components</p>
       </div>
       <div className='relative flex w-full p-5 flex-auto flex-col place-content-inherit align-items-inherit h-auto break-words text-left overflow-y-auto subpixel-antialiased'>
-        <p className='font-normal text-base text-default-500'>
+        <p className='text-slate-500 dark:text-slate-400 text-sm opacity-70'>
           All NextUI components already include the use client directive, which means you can import and use them directly in your RSC.
         </p>
       </div>
