c0ba1774ca3be2b277d67eb9bf6f10919966d5f3
Aviv Maman <awiwmaman@outlook.com>
2023-08-28 23:44:53 +0300

    Sidebar

---
+++ components/SidebarMenu.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { type FC } from 'react';
+
+interface SidebarMenuProps {}
+
+const SidebarMenu: FC<SidebarMenuProps> = ({}) => {
+  return <div>SidebarMenu</div>;
+};
+
+export default SidebarMenu;
