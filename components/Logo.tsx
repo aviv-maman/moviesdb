eb14735f4f87e35ca15b44d4a82fe993adba0db8
Aviv Maman <awiwmaman@outlook.com>
2023-09-05 00:20:24 +0300

    multi select css

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,5 @@
 import { Image } from '@nextui-org/react';
 
 export default function Logo() {
-  return <Image src='./logo.jpg' alt='Logo' className='inline-block h-8' />;
+  return <Image src='/logo.jpg' alt='Logo' className='inline-block h-8' />;
 }
--- components/MultiSelect.tsx
+++ components/MultiSelect.tsx
@@ -17,6 +17,14 @@ const MultiSelect: FC<MultiSelectProps> = ({ options }) => {
     { value: 'chocolate', label: 'Chocolate' },
     { value: 'strawberry', label: 'Strawberry' },
     { value: 'vanilla', label: 'Vanilla' },
+    { value: 'carrot', label: 'Carrot' },
+    { value: 'banana', label: 'Banana' },
+    { value: 'apple', label: 'Apple' },
+    { value: 'orange', label: 'Orange' },
+    { value: 'grape', label: 'Grape' },
+    { value: 'caramel', label: 'Caramel' },
+    { value: 'nuts', label: 'Nuts' },
+    { value: 'cookies', label: 'Cookies' },
   ];
 
   const filterOptions = (inputValue: string) => {
@@ -39,6 +47,20 @@ const MultiSelect: FC<MultiSelectProps> = ({ options }) => {
       placeholder='Type to search...'
       loadOptions={promiseOptions}
       openMenuOnClick={false}
+      className='font-normal text-small text-foreground-500'
+      styles={{
+        control: (baseStyles, { isFocused, isDisabled }) => ({
+          ...baseStyles,
+          backgroundColor: 'transparent',
+          border: isDisabled ? '2px solid rgb(156 163 175)' : isFocused ? '2px solid #17c964' : '2px solid #3f3f46',
+          minHeight: 40,
+          borderRadius: 10,
+          boxShadow: undefined,
+          '&:hover': {
+            borderColor: isFocused ? '2px solid #17c964' : '#71717a',
+          },
+        }),
+      }}
     />
   );
 };
