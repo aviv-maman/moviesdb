47419f96866db1d3b112e24bf295b2bbf0313099
Aviv Maman <awiwmaman@outlook.com>
2023-09-11 23:27:20 +0300

    Update SliderCustom.tsx

--- components/SliderCustom.tsx
+++ components/SliderCustom.tsx
@@ -3,7 +3,18 @@ import { type FC } from 'react';
 interface SliderCustomProps {}
 
 const SliderCustom: FC<SliderCustomProps> = ({}) => {
-  return <input type='range' min='0' max='10' />;
+  return (
+    <div className='h-6 w-full'>
+      <input type='range' min='0' max='10' className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700' />
+      <div className='w-full flex justify-between text-xs'>
+        {Array.from(Array(11).keys()).map((item) => (
+          <span key={item} className='text-gray-400 dark:text-gray-500'>
+            {item < 10 ? `0${item}` : item}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SliderCustom;
