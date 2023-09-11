9854ebc28c1f31fd49185b0fb030f07570534f62
Aviv Maman <awiwmaman@outlook.com>
2023-09-12 00:09:05 +0300

    Update SliderCustom.tsx

--- components/SliderCustom.tsx
+++ components/SliderCustom.tsx
@@ -5,7 +5,7 @@ interface SliderCustomProps {}
 const SliderCustom: FC<SliderCustomProps> = ({}) => {
   return (
     <div className='h-6 w-full'>
-      <input type='range' min='0' max='10' className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700' />
+      <input type='range' min='0' max='10' className='w-full cursor-grab' />
       <div className='w-full flex justify-between text-xs'>
         {Array.from(Array(11).keys()).map((item) => (
           <span key={item} className='text-gray-400 dark:text-gray-500'>
