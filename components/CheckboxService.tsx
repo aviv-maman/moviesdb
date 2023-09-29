8020f5e4775fe9e69b13d33e2a5a1a5a3b28f117
Aviv Maman <awiwmaman@outlook.com>
2023-09-29 23:07:57 +0300

    tooltip fix

--- components/CheckboxService.tsx
+++ components/CheckboxService.tsx
@@ -43,6 +43,7 @@ const CheckboxService: FC<CheckboxServiceProps> = (props) => {
     <Tooltip
       content={props.value}
       showArrow
+      placement='bottom'
       classNames={{
         base: 'py-3 px-4 border border-default-200 bg-gradient-to-br from-white to-default-300 dark:from-default-100 dark:to-default-50',
         arrow: 'bg-default-200',
