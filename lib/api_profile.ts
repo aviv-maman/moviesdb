7b1c55ef6a3d0fdbd80ba940b617e0fda75dc025
Aviv Maman <awiwmaman@outlook.com>
2024-05-07 18:45:28 +0300

    Update api_profile.ts

--- lib/api_profile.ts
+++ lib/api_profile.ts
@@ -1,3 +1,4 @@
+'use server';
 import type { CreateRequestTokenResponse, DeleteTmdbSessionIdResponse } from '@/lib/api.types';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
