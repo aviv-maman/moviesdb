ae0c9c65f0e7f89dba2db52b90a4845a9365920b
Aviv Maman <awiwmaman@outlook.com>
2025-01-10 09:48:04 +0200

    registry.tsx was removed

--- hooks/useLocalStorageState.ts
+++ hooks/useLocalStorageState.ts
@@ -1,35 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-const useLocalStorageState = (initialValue: string | number | boolean, key: string) => {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      // If error also return initialValue
-      console.log('useLocalStorageState Error', error);
-      return initialValue;
-    }
+function useLocalStorageState<T>(key: string, defaultValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [state, setState] = useState<T>(() => {
+    // Retrieve stored value from local storage or use default value
+    const storedValue = window.localStorage.getItem(key);
+    return storedValue ? (JSON.parse(storedValue) as T) : defaultValue;
   });
 
-  // useEffect to update local storage when the state changes
   useEffect(() => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore = typeof storedValue === 'function' ? storedValue(storedValue) : storedValue;
-      // Save state
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.log('useLocalStorageState Error', error);
-    }
-  }, [key, storedValue]);
+    // Store the state value in local storage whenever it changes
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
 
-  return [storedValue, setStoredValue];
-};
+  return [state, setState];
+}
 
 export default useLocalStorageState;
